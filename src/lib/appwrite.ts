df00ecc9812abc57fd9ab8024f88081c3cf65a5a
Ansell Maximilian <ansell.maximilian@nexsoft.co.id>
2024-07-11 15:14:40 +0700

    prepare for migration to next

--- src/lib/appwrite.ts
+++
@@ -1,7 +0,0 @@
-import { Client } from "appwrite";
-
-const client = new Client();
-
-client
-  .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject(String(import.meta.env.VITE_APPWRITE_PROJECT_ID));
