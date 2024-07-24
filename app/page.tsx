bdc49657b7d637d0c53cd6a924a98ea2b8c3373c
Ansell Maximilian <ansellmaximilian@gmail.com>
2024-07-24 19:39:59 +0700

    enhancement: Responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -7,27 +7,27 @@ import { cn } from "@/lib/utils";
 export default function Home() {
   return (
     <div className="min-h-screen flex-col flex bg-[url('/images/banter-bot-bg.png')] justify-center">
-      <main className="container mx-auto p-4 py-16">
+      <main className="container mx-auto p-4 py-8">
         <div className="flex flex-col justify-center items-center gap-8">
           <Image
             src={hero}
             width={350}
             height={350}
             alt="logo"
-            className="mx-auto"
+            className="mx-auto w-44 md:w-72"
           />
           <div>
-            <h1 className="text-3xl font-semibold text-center mb-2">
+            <h1 className="text-2xl md:text-3xl font-semibold text-center mb-2">
               Master a New Language Through Engaging Conversations!
             </h1>
-            <p className="text-2xl text-center">
+            <p className="text-xl md:text-2xl text-center">
               Practice with various AI Personalities and Experience Unique,
               Dynamic, and Responsive Interactions.
             </p>
           </div>
           <Link
             href="/app/dashboard"
-            className={cn(buttonVariants({}), "text-2xl px-8 py-6")}
+            className={cn(buttonVariants({}), "text-xl md:text-2xl px-8 py-6")}
           >
             Get Started
           </Link>
